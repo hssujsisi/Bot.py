cd0abc75a255857aba068f73a3fb440e05f0d4d7
hssujsisi <mohamadhosineamiry@gmail.com>
2025-06-04 11:35:48 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+import logging
+
+TOKEN = "7893391053:AAEylzmxixnX3t35F8o5ZJwBx9dJnvkCSS8"
+ADMIN_ID = "5040554415"
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def start(update, context):
+    update.message.reply_text(
+        "📚 بیاتوانیمه | از سال 1396\n"
+        "✉️ ارتباط: bia2anime.com@gmail.com\n\n"
+        "برای ثبت نام: /register"
+    )
+
+def register(update, context):
+    btn = [[KeyboardButton("ارسال شماره 📞", request_contact=True)]]
+    update.message.reply_text(
+        "لطفاً شماره خود را ارسال کنید:",
+        reply_markup=ReplyKeyboardMarkup(btn, resize_keyboard=True)
+    )
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(CommandHandler('register', register))
+updater.start_polling()
+updater.idle()
